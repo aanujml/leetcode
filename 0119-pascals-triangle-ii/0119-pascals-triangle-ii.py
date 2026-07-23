b1366712ed806775cf87938ba28b6c1c57e1c237
Anuj Kumar <anujk938654@gmail.com>
2026-07-23 15:12:09 +0530

    Time: 0 ms (100%), Space: 19.4 MB (21.45%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+        
+        for i in range(1, rowIndex + 1):
+            # Mathematical relationship between adjacent terms in Pascal's row
+            next_val = row[-1] * (rowIndex - i + 1) // i
+            row.append(next_val)
+            
+        return row
+        
